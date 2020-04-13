bc88a42a4acb47ad044a2f1790a758eaf3c33bdc
rafael <rafael.machado@nodo.cc>
2020-04-12 21:17:16 -0300

    Added end game state

--- app/src/main/java/com/example/amorient/firstscreen/FirstScreenActivity.kt
+++ app/src/main/java/com/example/amorient/firstscreen/FirstScreenActivity.kt
@@ -1,5 +1,7 @@
 package com.example.amorient.firstscreen
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.amorient.R
@@ -7,6 +9,10 @@ import com.example.amorient.start.StartScreenActivity
 import kotlinx.android.synthetic.main.activity_first_screen.*
 
 class FirstScreenActivity: AppCompatActivity() {
+
+    companion object {
+        fun launchIntent(context: Context) = Intent(context, FirstScreenActivity::class.java)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
